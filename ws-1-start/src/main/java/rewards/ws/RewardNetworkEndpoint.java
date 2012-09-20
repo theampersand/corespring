300096695688ebbbf825ed3255b823540f6bd4e5
Andrew Phillips <theampersand@gmail.com>
2012-09-20 18:04:43 -0400

    core spring training day 3

--- ws-1-start/src/main/java/rewards/ws/RewardNetworkEndpoint.java
+++ ws-1-start/src/main/java/rewards/ws/RewardNetworkEndpoint.java
@@ -2,8 +2,16 @@ package rewards.ws;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import rewards.AccountContribution;
+import rewards.Dining;
+import rewards.RewardConfirmation;
 import rewards.RewardNetwork;
+import rewards.ws.types.RewardAccountForDiningRequest;
+import rewards.ws.types.RewardAccountForDiningResponse;
 
 @Endpoint
 public class RewardNetworkEndpoint {
@@ -17,10 +25,34 @@ public class RewardNetworkEndpoint {
 		this.rewardNetwork = rewardNetwork;
 	}
 	
-	// TODO 3: Create a new method which accepts a RewardAccountForDiningRequest
+	// TODO 3: DONE Create a new method which accepts a RewardAccountForDiningRequest
 	// as unmarshalled request payload, processes the request and returns
 	// a RewardAccountForDiningResponse as the response payload to be marshalled
 	// for the appropriate payload root element. 
 
+	@PayloadRoot(localPart="rewardAccountForDiningRequest",
+				 namespace="http://www.springsource.com/reward-network")
+	@ResponsePayload
+	public RewardAccountForDiningResponse getRewardAccountForDiningResponse(@RequestPayload RewardAccountForDiningRequest request){
+		
+		RewardConfirmation rewardConfirmation;
+		if (null != request)
+		{			
+			 rewardConfirmation = 
+					rewardNetwork.rewardAccountFor(Dining.createDining(request.getAmount().toString(), request.getCreditCardNumber(), request.getMerchantNumber()));
+			 RewardAccountForDiningResponse rewardAccountForDiningResponse = new RewardAccountForDiningResponse();
+			 AccountContribution accountContribution = rewardConfirmation.getAccountContribution();
+			 
+			 if (null != accountContribution)
+			 {
+				 rewardAccountForDiningResponse.setAccountNumber(accountContribution.getAccountNumber());
+				 rewardAccountForDiningResponse.setAmount(accountContribution.getAmount().asBigDecimal());
+				 rewardAccountForDiningResponse.setConfirmationNumber(rewardConfirmation.getConfirmationNumber());
+			 }
+
+			 return rewardAccountForDiningResponse;
+		}
+		throw new IllegalArgumentException("Request is null");
+	}
 
 }
--- ws-1-start/src/test/java/rewards/ws/client/SoapRewardNetworkTests.java
+++ ws-1-start/src/test/java/rewards/ws/client/SoapRewardNetworkTests.java
@@ -3,9 +3,19 @@ package rewards.ws.client;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.StringWriter;
+
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,31 +26,36 @@ import org.springframework.ws.client.core.WebServiceTemplate;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-@ContextConfiguration(locations = {"classpath:rewards/ws/client/client-config.xml"})
+@ContextConfiguration(locations = { "classpath:rewards/ws/client/client-config.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SoapRewardNetworkTests {
-	
+
 	private static final String NAMESPACE_URI = "http://www.springsource.com/reward-network";
 
 	@Autowired
 	private WebServiceTemplate webServiceTemplate;
-	
+
 	@Test
 	public void testWebServiceWithJAXB() throws Exception {
-		//TODO 5: Implement this method by using your generated JAXB2 classes
+		// TODO 5: Implement this method by using your generated JAXB2 classes
 	}
-	
+
 	@Test
 	public void testWebServiceWithXml() throws Exception {
-		Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-		Element requestElement = document.createElementNS(NAMESPACE_URI, "rewardAccountForDiningRequest");
+		Document document = DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder().newDocument();
+		Element requestElement = document.createElementNS(NAMESPACE_URI,
+				"rewardAccountForDiningRequest");
 		requestElement.setAttribute("amount", "100.00");
 		requestElement.setAttribute("creditCardNumber", "1234123412341234");
 		requestElement.setAttribute("merchantNumber", "1234567890");
-		DOMSource source = new DOMSource(requestElement);
-		DOMResult result = new DOMResult();
+
+		System.out.println(xmlToString(requestElement));
+		
+		Source source = new DOMSource(requestElement);
+		Result result = new DOMResult();
 		webServiceTemplate.sendSourceAndReceiveToResult(source, result);
-		Element responseElement = (Element) result.getNode().getFirstChild();
+		Element responseElement = (Element) ((DOMResult) result).getNode().getFirstChild();
 		
 		// assert the expected reward confirmation results
 		assertNotNull(responseElement);
@@ -50,9 +65,20 @@ public class SoapRewardNetworkTests {
 		
 		// the total contribution amount should be 8.00 (8% of 100.00)
 		assertEquals("8.00", responseElement.getAttribute("amount"));
+		
+		
 	}
-		
-		
-	
+
+	private String xmlToString(Element requestElement)
+			throws TransformerFactoryConfigurationError,
+			TransformerConfigurationException, TransformerException {
+		Source source = new DOMSource(requestElement);
+		StringWriter stringWriter = new StringWriter();
+		Result result = new StreamResult(stringWriter);
+		TransformerFactory factory = TransformerFactory.newInstance();
+		Transformer transformer = factory.newTransformer();
+		transformer.transform(source, result);
+		return (stringWriter.getBuffer().toString());
+	}
 
 }
